e8468272e68640d3ae72e62f28979ac9b9f7bbf7
Richie Lee <richie@bzzzt.io>
2022-07-07 16:31:12 +0100

    pkmn ascii files

--- Public/Get-PokemonAsAscii.ps1
+++ Public/Get-PokemonAsAscii.ps1
@@ -5,6 +5,7 @@ Function Get-PokemonAsAscii {
         $Pokemon
     )
     $uponelevel = Split-Path (Split-Path $PSCommandPath)
-    $pkmnAsciiFiles = Join-Path -Path $uponelevel -ChildPath (Join-Path -Path $pokemon -ChildPath ".txt")
+    $pkmnAsciiFiles = Join-Path -Path $uponelevel -ChildPath (Join-Path -Path (Join-Path 'pkmnasciifiles' $pokemon) -ChildPath ".txt")
     Test-Path $pkmnAsciiFiles
+    Write-Host $pkmnAsciiFiles
 }
